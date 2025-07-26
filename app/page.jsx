0db2dbfbbe40dffb6ddf95cbffd521441486e711
arifbillah0156 <arifbillah0156@gmail.com>
2025-07-26 22:12:51 +0600

    abeditas45df456fdafvz

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,32 @@
 import HomeAddBiodata from "@/components/HomeAddBiodata";
+import AdvisePage from "@/components/HomeAdvise";
+import HadithPage from "@/components/HomeHadith";
+import FunnyMarriage from "@/components/HomeMarriageTips";
 import HomePageText from "@/components/HomePageText";
 import HomeSearchForm from "@/components/HomeSearchForm";
 
 export default function Home() {
   return (
-    <div
-      className="h-max w-[100%] bg-cover bg-center pb-24 mb-[-41px] rounded-3xl"
-      style={{
-        backgroundImage: `url(/Images/BG5.jpg)`,
-      }}
-    >
-      <HomePageText />
+    <>
+      <section
+        className="h-max w-[100%] bg-cover bg-center pb-24 mb-[-41px] rounded-3xl"
+        style={{
+          backgroundImage: `url(/Images/BG5.jpg)`,
+        }}
+      >
+        <HomePageText />
 
-      <HomeAddBiodata />
+        <HomeAddBiodata />
 
-      <HomeSearchForm />
-    </div>
+        <HomeSearchForm />
+      </section>
+      {/*  */}
+
+      <HadithPage />
+
+      <AdvisePage />
+
+      <FunnyMarriage />
+    </>
   );
 }
